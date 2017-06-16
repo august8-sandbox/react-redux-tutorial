0685a8f6c1f91eabbc38a0154aa6f4b8ec5a7458
August8 <hazuki@people.co.jp>
2017-06-16 13:31:40 +0900

    階層を整理した。

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-
-import Counter from './Counter/App';
-import couterReducer from './Counter/reducers/CounterReducer';
+import Counter from './App';
+import couterReducer from './reducers/CounterReducer';
 
 const store = createStore(combineReducers({
   calc: couterReducer,
